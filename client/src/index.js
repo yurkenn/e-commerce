7216485d00c0ecce0f8dcd66ca2fc07bb2b6a6a8
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-04-30 16:54:39 +0300

    Fixed

--- client/src/index.js
+++ client/src/index.js
@@ -41,12 +41,3 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-// <QueryClientProvider client={queryClient}>
-// 	<ChakraProvider>
-// 		<AuthProvider>
-// 			<BasketProvider>
-// 				<App />
-// 			</BasketProvider>
-// 		</AuthProvider>
-// 	</ChakraProvider>
