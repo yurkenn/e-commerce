2de301477f18d42da0e9ed2d11c886e41d3d5308
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-04-30 10:54:48 +0300

    Add new routes

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,8 @@ import Signup from './pages/Auth/Signup';
 import Navbar from './components/Navbar';
 import Profile from './pages/Profile';
 import ProtectedRoute from './pages/ProtectedRoute';
+import Basket from './pages/Basket';
+import Eror404 from './pages/Eror404';
 
 function App() {
   return (
@@ -20,7 +22,9 @@ function App() {
             <Route path="/product/:product_id" component={ProductDetail} />
             <Route path="/signin" component={Signin} />
             <Route path="/signup" component={Signup} />
+            <Route path="/basket" component={Basket} />
             <ProtectedRoute path="/profile" component={Profile} />
+            <Route path="*" component={Eror404} />
           </Switch>
         </div>
       </div>
