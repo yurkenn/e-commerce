e39988858adeded8122046465adc3a75ac9a719b
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-04-30 16:55:57 +0300

    Add admin route

--- client/src/pages/ProtectedRoute.js
+++ client/src/pages/ProtectedRoute.js
@@ -2,12 +2,22 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 
-const ProtectedRoute = ({ component: Component, ...rest }) => {
-  const { loggedIn } = useAuth();
+const ProtectedRoute = ({ component: Component, admin, ...rest }) => {
+  const { loggedIn, user } = useAuth();
   return (
     <Route
       {...rest}
-      render={(props) => (loggedIn ? <Component {...props} /> : <Redirect to="/signin" />)}
+      render={(props) => {
+        if (admin && user?.role !== 'admin') {
+          return <Redirect to="/" />;
+        }
+
+        if (loggedIn) {
+          return <Component {...props} />;
+        }
+
+        return <Redirect to="/signin" />;
+      }}
     />
   );
 };
